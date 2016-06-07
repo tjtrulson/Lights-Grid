ecc6e3e2b874e065bf8dfc09a5c8fd424bb98aa7
tjtrulson <tjtrulson@gmail.com>
2016-06-07 11:42:31 -0400

    Unity Project Files
    Adding the project files from the Unity project on my desktop

---
+++ Assets/GridToggle.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GridToggle : MonoBehaviour {
+    //2D Array of buttons for the grid
+    private GameObject[,] grid = new GameObject[10,10];
+    //Temp array for setting the grid by finding the buttons in a row
+    private GameObject[] temp;
+    //2D List for the rectangle to be lit up
+    public List<GameObject> rectangle;
+
+    //First and last positions of the rectangle
+    private Lightbulb firstPos;
+    private Lightbulb lastPos;
+    
+    //Bool for if the first position of the rectangle has been set
+    private bool firstPosSet = false;
+
+    //Reset the rectangle to avoid incorrect changes
+    private void resetRect()
+    {
+        rectangle.Clear();
+    }
+
+    //This will be used to set either the starting position or ending position of the rectangle
+    public void SetPos()
+    {
+        //Set the currently selected light's Lightbulb script to the temp variable
+        Lightbulb tempLight = EventSystem.current.currentSelectedGameObject.GetComponent<Lightbulb>();
+        //If this click is to set the first position
+        if (!firstPosSet)
+        {
+            //Reset the previous rectangle for insurance
+            resetRect();
+            //Set firstPos to the temp variable
+            firstPos = tempLight;
+            //Highlight the selected light in green
+            firstPos.highlight();
+            //Update the bool for the second click
+            firstPosSet = true;
+        }
+        //If this click is to set the second position
+        else
+        {
+            //Set lastPos to the temp variable
+            lastPos = tempLight;
+            //Form the rectangle from the points between first and last pos
+            formRectangle(firstPos, lastPos);
+            //Update the bool for the first click
+            firstPosSet = false;
+        }
+    }
+
+    //This function forms the rectangle between the selected points
+    private void formRectangle(Lightbulb firstPos, Lightbulb lastPos)
+    {
+        //For sake of ease, swap the first/last positions when going to the left
+        if (lastPos.xPos < firstPos.xPos)
+        {
+            Lightbulb temp = firstPos;
+            firstPos = lastPos;
+            lastPos = temp;
+        }
+        //This loop only accounts for if lastPos.yPos >= firstPos.yPos (so down and to the right from starting pos)
+        if (lastPos.yPos >= firstPos.yPos)
+        {
+            for (int i = firstPos.yPos; i <= lastPos.yPos; i++)
+            {
+                for (int j = firstPos.xPos; j <= lastPos.xPos; j++)
+                {
+                    //Loop through the grid array, and add the lights between the first and last positions to the rectangle List
+                    rectangle.Add(grid[i, j]);
+                }
+            }
+        }
+        //This loop accounts for if lastPos.yPos < firstPos.yPos (so up and to the right from starting pos)
+        else if(lastPos.yPos < firstPos.yPos)
+        {
+            for (int i = firstPos.yPos; i >= lastPos.yPos; i--)
+            {
+                for (int j = firstPos.xPos; j <= lastPos.xPos; j++)
+                {
+                    //Loop through the grid array, and add the lights between the first and last positions to the rectangle List
+                    rectangle.Add(grid[i, j]);
+                }
+            }
+        }
+        //For the selected list, toggle the lights between the states
+        toggleLights();
+    }
+
+    //This function loops through the list and toggles the lights to the varying states
+    private void toggleLights()
+    {
+        foreach (GameObject button in rectangle)
+        {
+            //Call each button's toggle function
+            button.GetComponent<Lightbulb>().toggleLight();
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 10; i++)
+        {
+            //Find the buttons in the given row -- Sorted alphabetically (since FindGameObjectsWithTag adds them randomly)
+            temp = GameObject.FindGameObjectsWithTag("Row " + (i + 1)).OrderBy(go =>go.name).ToArray();
+            for (int j = 0; j < 10; j++)
+            {
+                //Add them to the grid
+                grid[i,j] = temp[j];
+            }
+        }
+	}
+}
---
+++ Assets/Lightbulb.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+//The three states for the lights
+public enum states
+{
+    off,
+    half,
+    on
+}
+public class Lightbulb : MonoBehaviour {
+
+    //Enum for the light's current state -- off is default
+    private states currentState = states.off;
+    
+    //Ints for the grid position of the given light
+    public int xPos;
+    public int yPos;
+
+    //Bool to determine if the light is active or not
+    private bool activated = false;
+
+    //Toggle lights on/off
+    public void toggleLight()
+    {
+        //When starting with a light that's off
+        if (currentState == states.off)
+        {
+            //Change to half-on
+            currentState = states.half;
+            this.GetComponent<Image>().color = Color.grey;
+        }
+        //When starting with a light that's half-on
+        else if (currentState == states.half)
+        {
+            //Change to full-on
+            currentState = states.on;
+            this.GetComponent<Image>().color = Color.white;
+        }
+        //When starting with a light that's fully on
+        else if (currentState == states.on)
+        {
+            //Change to off
+            currentState = states.off;
+            this.GetComponent<Image>().color = Color.black;
+        }
+    }
+
+    //Highlight selected light
+    public void highlight()
+    {
+        //Green just to differentiate it from the other lights
+        this.GetComponent<Image>().color = Color.green;
+    }
+}
